0ab7497fd70ad8a20f7ff9b1d2ea9b0970b48709
lior gishry <liorgish@gmail.com>
2019-05-09 17:15:17 +0300

    more

--- lib/widgets/login-widgets/login.widget.dart
+++ lib/widgets/login-widgets/login.widget.dart
@@ -4,35 +4,47 @@ import 'package:funkinator/widgets/login-widgets/facebook.widget.dart';
 import 'package:funkinator/widgets/login-widgets/google.widget.dart';
 import 'package:funkinator/widgets/login-widgets/manual.widget.dart';
 
-
 class LoginWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GoogleLoginWidget(),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: FacebookLoginWidget(),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 0,vertical: 8),
-          child: Text(
-            DemoLocalizations.of(context).or,
-            style: TextStyle(fontSize: 20.0),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints viewportConstraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: viewportConstraints.maxHeight,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GoogleLoginWidget(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FacebookLoginWidget(),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+                  child: Text(
+                    DemoLocalizations.of(context).or,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ManualLoginWidget(),
+                )
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ManualLoginWidget(),
-        ), //
-      ],
+        );
+      },
     );
+    ;
   }
 }
--- lib/widgets/main-widgets/main-widget.dart
+++ lib/widgets/main-widgets/main-widget.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:funkinator/l10n/bl.dart';
-class MainGridWidget extends StatelessWidget{
+
+class MainGridWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
-
       children: <Widget>[
         GridView.count(
           shrinkWrap: true,
@@ -17,24 +17,38 @@ class MainGridWidget extends StatelessWidget{
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(onPressed: (){}, child: Text(DemoLocalizations.of(context).create_training)),
+              child: RaisedButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                  child: Text(DemoLocalizations.of(context).create_training)),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(onPressed:  (){}, child: Text(DemoLocalizations.of(context).manage_trainings)),
+              child: RaisedButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+
+                  child: Text(DemoLocalizations.of(context).manage_trainings)),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(onPressed:  (){}, child: Text(DemoLocalizations.of(context).start_training)),
+              child: RaisedButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+
+                  child: Text(DemoLocalizations.of(context).start_training)),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(onPressed:  (){}, child: Text(DemoLocalizations.of(context).statistics)),
+              child: RaisedButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+
+                  child: Text(DemoLocalizations.of(context).statistics)),
             ),
           ],
         ),
       ],
     );
   }
-
 }
